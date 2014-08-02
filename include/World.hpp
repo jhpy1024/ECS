136d2f09657c17ebeb41ef5aadeb02a6c1a637e0
jhpy1024 <jhpy1024@gmail.com>
2014-08-02 19:55:18 +0100

    World now has functions to add and get components

--- include/World.hpp
+++ include/World.hpp
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <string>
+#include <iostream>
 #include <unordered_map>
 
 #include "BaseComponent.hpp"
@@ -17,8 +18,70 @@ class World
 
         unsigned createEntity();
         void removeEntity(unsigned id);
-        bool hasEntity(unsigned id) const;
         void removeAllEntities();
+        bool hasEntity(unsigned id) const;
+
+        template <typename T>
+        std::shared_ptr<BaseComponent> addComponent(unsigned id)
+        {
+            if (!hasEntity(id))
+            {
+                std::cerr << "[Error] Unable to add component to entity as it does not exist"
+                          << " so returning nullptr." << std::endl;
+                return nullptr;
+            }
+
+            auto component = std::make_shared<T>();
+            m_Entities[id][component.ID] = component;
+
+            return component;
+        }
+
+        template <typename T>
+        bool hasComponent(unsigned id)
+        {
+            if (!hasEntity(id))
+            {
+                std::cerr << "[Error] Unable to check if entity has that component as the entity does not exist." << std::endl;
+                return false;
+            }
+
+            auto targetComponentID = T().ID;
+            return m_Entities[id].count(targetComponentID) > 0;
+        }
+
+        template <typename First, typename Second, typename... Others>
+        bool hasComponents(unsigned id)
+        {
+            return (hasComponent<First>(id) && hasComponents<Second, Others...>(id));
+        }
+
+        template <typename T>
+        std::shared_ptr<BaseComponent> getComponent(unsigned id)
+        {
+            auto targetComponentID = T().ID;
+
+            if (!hasEntity(id))
+            {
+                std::cerr << "[Error] Unable to get component with ID \"" << targetComponentID << "\" because the entity does not exist"
+                          << " so returning nullptr." << std::endl;
+                return nullptr;
+            }
+
+            if (!hasComponent<T>(id))
+            {
+                std::cerr << "[Error] Unable to get component with ID \"" << targetComponentID << "\" because it does not exist" 
+                          << " so returning nullptr." << std::endl;
+                return nullptr;
+            }
+
+            for (auto& componentMap : m_Entities[id])
+            {
+                auto currentComponentID = componentMap.first;
+                if (currentComponentID == targetComponentID)
+                    return componentMap.second;
+            }
+        }
 
     private:
         EntityMap m_Entities;
