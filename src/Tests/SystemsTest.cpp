abad21df29cdf2eecba392046ea1e7bed04ae0d7
jhpy1024 <jhpy1024@gmail.com>
2014-08-03 12:20:02 +0100

    Finished systems test

--- src/Tests/SystemsTest.cpp
+++ src/Tests/SystemsTest.cpp
@@ -3,6 +3,14 @@
 #include "World.hpp"
 #include "BaseSystem.hpp"
 
+struct PosComp : BaseComponent
+{
+    PosComp() : BaseComponent("Pos") {} 
+
+    int x;
+    int y;
+};
+
 class TestSystem : public BaseSystem
 {
     public:
@@ -14,6 +22,13 @@ class TestSystem : public BaseSystem
         void update(float delta, EntityMap& entities) override
         {
             std::printf("update() - %lu entities\n", entities.size());
+
+            for (auto& pair : entities)
+            {
+                auto pos = std::static_pointer_cast<PosComp>(pair.second["Pos"]);
+                pos->x += 10;
+                pos->y += 20;
+            }
         }
 
         void draw(EntityMap& entities) override
@@ -26,11 +41,15 @@ int main()
 {
     World world;
     world.registerSystem<TestSystem>();
-    world.createEntity();
+
+    auto entity = world.createEntity();
+    auto position = world.addComponent<PosComp>(entity);
+    std::printf("Position before (%d,%d)\n", position->x, position->y);
 
     world.handleInput();
     world.update(1.f);
     world.draw();
 
+    std::printf("Position after (%d,%d)\n", position->x, position->y);
     return 0;
 }
